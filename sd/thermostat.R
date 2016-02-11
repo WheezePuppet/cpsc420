eac89e3dbc41e086b0f0287de0000088e4fa04d9
Stephen <stephen@umw.edu>
2016-02-11 14:55:36 -0800

    Include varying outside temperature.

--- sd/thermostat.R
+++ sd/thermostat.R
@@ -16,13 +16,19 @@ ttoi <- function(t) (t-starting.time)/delta.t + 1
 # Simulation parameters (inputs).
 thermostat <- 75     # degF
 heating.rate <- 6    # degF/hr
-outside <- 39        # degF
-ins.loss.rate <- .1  # (degF/hr)/degF
-hysteresis.factor <- 5 # degF
+
+# Cyclical temperature pattern with some random noise.
+outside <- sin(2*pi*time/24) * 20 + 35 + rnorm(length(time), mean=0, sd=20)
+
+# 2-hour long smallish reverse nuclear explosion at Wednesday noon
+outside[ttoi(3*24+12):ttoi(3*24+14)] <- -400
+
+ins.loss.rate <- .05  # (degF/hr)/degF
+hysteresis.factor <- 1 # degF
 
 # Stocks. (Create a vector and an initial condition for each.)
 room.temp <- vector(length=length(time))
-room.temp[1] <- outside
+room.temp[1] <- outside[1]
 furnace.on <- vector(length=length(time))
 furnace.on[1] <- FALSE
 
@@ -54,7 +60,7 @@ for (i in 2:length(time)) {
     }
 
     # neg value = colder inside than out
-    discrepancy.outside <- room.temp[i-1] - outside     # degF
+    discrepancy.outside <- room.temp[i-1] - outside[i-1]     # degF
     leakage <- ins.loss.rate * discrepancy.outside      # degF/hr
 
     # Compute the values of all the derivatives of the stocks ("primes").
@@ -66,11 +72,11 @@ for (i in 2:length(time)) {
 
 # Plot and analyze.
 all.values <- c(outside,room.temp,thermostat)
-plot(time,room.temp,type="l",col="red",lwd=2,
-    xlab="hrs",ylab=~degree~F,main="Temperature over time",
+plot(time/24,room.temp,type="l",col="red",lwd=2,
+    xlab="days",ylab=~degree~F,main="Temperature over time",
     ylim=range(all.values))
 abline(h=thermostat,lty="dashed",col="black")
-abline(h=outside,lty="dotted",lwd=2,col="blue")
+lines(time/24,outside,lty="dotted",lwd=.5,col="blue")
 
 cat("Btw, the furnace was on ", sum(furnace.on)/length(furnace.on)*100,
     "% of the time.\n",sep="")
