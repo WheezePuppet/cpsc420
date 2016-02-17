242a5298d62fa9c13dfc2837aa0a3911ca3a2f94
Stephen <stephen@umw.edu>
2016-02-17 09:35:13 -0800

    Response delay parameter sweep.

--- sd/cars.R
+++ sd/cars.R
@@ -1,6 +1,6 @@
 
-# Meadows ch.2 Car dealership example (Fig. 29) -- version 1.2
-# delivery and response delays
+# Meadows ch.2 Car dealership example (Fig. 29) -- version 2.0
+# response delay parameter sweep
 # CPSC 420 -- spring 2016
 
 # Set up time. (delta.t and time vector).
@@ -22,7 +22,6 @@ sales <- vector()     # cars/day
 sales[1] <- 0
 
 delivery.delay <- 1   # days
-response.delay <- 1   # days
 
 # Stocks. (Create a vector and an initial condition for each.)
 inventory <- vector(length=length(time))
@@ -30,34 +29,45 @@ inventory[1] <- 150
 
 # Simulation loop. 
 # For each slice of simulated time,
-for (i in 2:length(time)) {
+results <- data.frame()
+for (response.delay in 0:10) {
+    for (i in 2:length(time)) {
 
-    # Compute the values of all the flows, based on previous stock values.
+        # Compute the values of all the flows, based on previous stock 
+        # values.
 
-    sales[i] <- customer.demand * inventory[i-1]
-    perceived.sales <- sales[i]   # NO perception delay.
+        sales[i] <- customer.demand * inventory[i-1]
+        perceived.sales <- sales[i]   # NO perception delay.
 
-    desired.inventory <- 
-        min(lot.size, perceived.sales * stocking.factor)
-    discrepancy <- inventory[i-1] - desired.inventory # pos = enough
+        desired.inventory <- 
+            min(lot.size, perceived.sales * stocking.factor)
+        discrepancy <- inventory[i-1] - desired.inventory # pos = enough
 
-    orders[i] <- max(0,-discrepancy/(response.delay+1)) / delta.t
+        orders[i] <- max(0,-discrepancy/(response.delay+1)) / delta.t
 
-    deliveries <- orders[max(1,i-delivery.delay)]
+        deliveries <- orders[max(1,i-delivery.delay)]
 
-    # Compute the values of all the derivatives of the stocks ("primes").
-    inventory.prime <- deliveries - sales[i]
+        # Compute the values of all the derivatives of the stocks 
+        # ("primes").
+        inventory.prime <- deliveries - sales[i]
 
-    # Compute all the new stock values (including any derived stocks).
-    inventory[i] <- inventory[i-1] + inventory.prime * delta.t
-
+        # Compute all the new stock values (including any derived 
+        # stocks).
+        inventory[i] <- inventory[i-1] + inventory.prime * delta.t
+    }
+    results <- rbind(results,
+        list(response.delay=response.delay,
+            total.sales=sum(sales),
+            inventory.wildness=sd(inventory)
+        )
+    )
 }
 
 # Plot and analyze.
-all.values <- c(0,inventory,orders,sales)
-plot(time,inventory,type="l",col="red",lwd=2,ylim=range(all.values),
-    xlab="days",ylab="",main="The local car dealership")
-lines(time,orders,col="purple",lwd=2)
-lines(time,sales,col="darkgreen",lwd=2)
-legend("bottomright",fill=c("purple","red","darkgreen"),
-    legend=c("orders","inventory","sales"))
+all.vals <- c(results$total.sales, results$inventory.wildness, 0)
+plot(results$response.delay, results$total.sales, type="l", col="darkgreen",
+    lwd=2, main="Total sales and wildness of inventory vs. response delay",
+    xlab="response delay (days)", ylab="", ylim=range(all.vals))
+lines(results$response.delay, results$inventory.wildness*10, col="red", lwd=2)
+legend("top", fill=c("darkgreen","red"), 
+    legend=c("total sales ($)", "10 x Std. dev. of inventory"))
