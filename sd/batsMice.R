830278f001e4d2eb5d1b8c699ed7b0d3ad818e57
Stephen <stephen@umw.edu>
2016-02-23 19:36:14 -0800

    Add parallel parameter sweep.

--- sd/batsMice.R
+++ sd/batsMice.R
@@ -1,5 +1,9 @@
 
-# Shiflet 4.2 -- Lotka-Volterra predator/prey model
+library(doParallel)
+registerDoParallel(8)
+
+force.regenerate.results <- FALSE
+time.delay.animation <- .1         # seconds
 
 # Set up time. (delta.t and time vector).
 delta.t <- 1/30 # months
@@ -25,35 +29,76 @@ M[1] <- 200
 B <- vector(length=length(time))
 B[1] <- 15
 
-# Simulation loop. 
-# For each slice of simulated time,
-for (i in 2:length(time)) {
+# Simulation loop. Only generate these time-consuming results if have to or
+# we're explicitly instructed to.
+if (!exists("grand.results")  ||  force.regenerate.results) {
 
-    # Compute the values of all the flows, based on previous stock values.
-    mouse.births <- mouse.birth.rate * M[i-1]
-    bat.births <- bat.birth.rate * B[i-1]
+  # Remember what time this started, just so we can brag about our performance
+  # by running in parallel.
+  first <- proc.time()
 
-    encounter.rate <- encounter.freq * M[i-1] * B[i-1]   # enc/month
-    kill.rate <- kill.ratio * encounter.rate             # kill/month
+  grand.results <- 
+    foreach (mouse.birth.rate = seq(1,2,.005), .combine=rbind) %dopar% {
 
-    bat.deaths <- bat.death.rate * B[i-1] - nutrition.factor * kill.rate
-    mouse.deaths <- mouse.death.rate * M[i-1] + kill.rate * mice.per.kill
+        one.birth.rate.results <- data.frame()
+        for (nutrition.factor in seq(0,5,.1)) {
+            for (i in 2:length(time)) {
 
-    # Compute the values of all the derivatives of the stocks ("primes").
-    M.prime <- mouse.births - mouse.deaths
-    B.prime <- bat.births - bat.deaths
+                mouse.births <- mouse.birth.rate * M[i-1]
+                bat.births <- bat.birth.rate * B[i-1]
 
-    # Compute all the new stock values (including any derived stocks).
-    M[i] <- M[i-1] + M.prime * delta.t
-    B[i] <- B[i-1] + B.prime * delta.t
+                encounter.rate <- encounter.freq * M[i-1] * B[i-1] # enc/month
+                kill.rate <- kill.ratio * encounter.rate           # kill/month
+
+                bat.deaths <- bat.death.rate * B[i-1] - 
+                    nutrition.factor * kill.rate
+                mouse.deaths <- mouse.death.rate * M[i-1] + 
+                    kill.rate * mice.per.kill
+
+                M.prime <- mouse.births - mouse.deaths
+                B.prime <- bat.births - bat.deaths
+
+                M[i] <- M[i-1] + M.prime * delta.t
+                B[i] <- B[i-1] + B.prime * delta.t
+            }
+            one.birth.rate.results <- rbind(one.birth.rate.results, 
+                data.frame(mouse.birth.rate=mouse.birth.rate,
+                           nutrition.factor=nutrition.factor,
+                    min.bats=min(B),
+                    min.mice=min(M)))
+        }
+        return(one.birth.rate.results)
+    }
+
+  # Print the elapsed time (current time minus what it was when we started).
+  print(proc.time() - first)
 }
 
-# Plot and analyze.
-all.vals <- c(M,B,0)
-plot(time,M,type="l",col="black",lwd=2,
-    ylim=range(all.vals), xlab="time (months)", main="Bats vs. mice")
-lines(time,B,col="red",lwd=3)
-legend("topleft",fill=c("black","red"),legend=c("mice","bats"))
+# (Relabel data frame rows because it's just too annoying otherwise.)
+rownames(grand.results) <- 1:nrow(grand.results)
 
-cat("The bats got as low as",min(B),"\n")
-cat("The mice got as low as",min(M),"\n")
+
+# Plot as an animation. Each frame of the animation corresponds to a
+# particular value of mouse.birth.rate, and plots nutrition.factor vs. our two
+# population minima (mice, and bats).
+for (mouse.birth.rate in unique(grand.results$mouse.birth.rate)) {
+
+    # Get just the results for the current mouse.birth.rate.
+    the.results <- 
+        grand.results[grand.results$mouse.birth.rate == mouse.birth.rate,]
+
+    # Plot our two dependent variables (simulation outputs) versus our
+    # simulation parameter (simulation input). Use a constant ylim so the axes
+    # scales don't change from frame to frame, which would confuse us.
+    plot(the.results$nutrition.factor,the.results$min.bats,type="l",
+        col="red",lwd=2,
+        ylim=c(0,50), xlab="nutrition.factor (bats/kill)",
+        main=paste("Bats vs. mice: mouse.birth.rate=",mouse.birth.rate))
+    lines(the.results$nutrition.factor,the.results$min.mice,col="black",
+        lwd=2)
+    legend("topleft",fill=c("black","red"),legend=c("mice","bats"))
+
+    # Delay between animation frames.
+    Sys.sleep(time.delay.animation)
+}
+
