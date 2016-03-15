2f5057684ca5161ee5c9674371ae6eaf2379f7f0
Stephen <stephen@umw.edu>
2016-03-15 06:21:19 -0700

    In-class version from 3/10.

--- ca/life.R
+++ ca/life.R
@@ -4,165 +4,150 @@
 
 # Possible states of CA cells.
 EMPTY <- 0
-POPULATED <- 1
+POP <- 1
 
 # Dimensions of CA.
 height <- 30
 width <- 30
 num.gen <- 100
 
+
+# A static starting configuration. 
+config <- matrix(c(
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+byrow=TRUE,nrow=height)
+
+# A random starting configuration.
+config <- matrix(rbinom(height*width,size=1,prob=.15))
+
 # Our system's state.
-config <- matrix(
-    c(
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    ), nrow=30, byrow=TRUE)
-da.cube <- array(EMPTY,dim=c(height,width,num.gen))
-da.cube[,,1] <- config
+da.loaf <- array(EMPTY, dim=c(height,width,num.gen))
+da.loaf[,,1] <- config
 
 
-# Return the number of neighbors that a particular cell has in a specified
-# generation. If the cell is on the grid border, do not count its phantom
-# cells over the boundary as "neighbors."
-num.neighbors <- function(row,col,gen) {
-    n <- 0
-    # above
-    if (row > 1  &&  da.cube[row-1,col,gen] == POPULATED) {  
-        n <- n + 1
+# Return the number of populated neighbors of (row,col) in the current
+# generation. (If (row,col) is a border cell of the grid, assume its
+# non-existent across-the-border neighbors are *not* populated.)
+num.neighbros <- function(da.loaf,row,col,gen) {
+    bros <- 0
+    if (row > 1 && col > 1 && da.loaf[row-1,col-1,gen] == POP) {
+        bros <- bros + 1
     }
-    # above left
-    if (row > 1 && col > 1  && da.cube[row-1,col-1,gen] == POPULATED) {
-        n <- n + 1
+    if (row > 1 && da.loaf[row-1,col,gen] == POP) {
+        bros <- bros + 1
     }
-    # above right
-    if (row > 1 && col < width  && da.cube[row-1,col+1,gen] == POPULATED) {
-        n <- n + 1
+    if (row > 1 && col < width && da.loaf[row-1,col+1,gen] == POP) {
+        bros <- bros + 1
     }
-    # left
-    if (col > 1  && da.cube[row,col-1,gen] == POPULATED) {
-        n <- n + 1
+    if (col > 1 && da.loaf[row,col-1,gen] == POP) {
+        bros <- bros + 1
     }
-    # right
-    if (col < width  && da.cube[row,col+1,gen] == POPULATED) {
-        n <- n + 1
+    if (col < width && da.loaf[row,col+1,gen] == POP) {
+        bros <- bros + 1
     }
-    # below 
-    if (row < height && da.cube[row+1,col,gen] == POPULATED) {
-        n <- n + 1
+    if (row < height && col > 1 && da.loaf[row+1,col-1,gen] == POP) {
+        bros <- bros + 1
     }
-    # below left
-    if (col > 1  && row < height && da.cube[row+1,col-1,gen] == POPULATED) {
-        n <- n + 1
+    if (row < height && da.loaf[row+1,col,gen] == POP) {
+        bros <- bros + 1
     }
-    # below right
-    if (col < width  && row < height && da.cube[row+1,col+1,gen] == POPULATED) {
-        n <- n + 1
+    if (row < height && col < width && da.loaf[row+1,col+1,gen] == POP) {
+        bros <- bros + 1
     }
-    n
+    return(bros)
 }
 
-
-# Return true only if the cell whose row and column are specified (which
-# was presumably populated in the previous generation) should stay
-# populated in the current generation.
-stay.alive <- function(row,col,prevGen) {
-    nn <- num.neighbors(row,col,prevGen)
-    if (nn == 2 || nn == 3) {
-        return(T)
-    } else {
-        return(F)
-    }
+# Return true if the cell in (row,col) of the previous generation was
+# populated.
+was.just.alive <- function(da.loaf,row,col,current.gen) {
+    return (da.loaf[row,col,current.gen-1] == POP)
 }
 
-
-# Return true only if the cell whose row and column are specified (which
-# was presumably empty in the previous generation) should become populated 
-# in the current generation.
-spawn <- function(row,col,prevGen) {
-    nn <- num.neighbors(row,col,prevGen)
-    if (nn == 3) {
-        return(T)
-    } else {
-        return(F)
-    }
+# Return true if the (presumably populated) cell in (row,col) of the previous
+# generation should stay populated in the current generation.
+should.stay.alive <- function(da.loaf,row,col,current.gen) {
+    return (num.neighbros(da.loaf,row,col,current.gen-1) %in% c(2,3))
 }
 
+# Return true if the (presumably empty) cell in (row,col) of the previous
+# generation should become populated in the current generation.
+should.spawn <- function(da.loaf,row,col,current.gen) {
+    return (num.neighbros(da.loaf,row,col,current.gen-1) == 3)
+}
 
-# Return a list of x-coordinates and corresponding y-coordinates for the
-# points whose cells in the specific generation are in a certain state
-# (default POPULATED).
-points.for.grid <- function(gen, value=POPULATED) {
+# Plot the grid for the generation specified.
+plot.gen <- function(da.loaf,gen) {
     xcoords <- vector()
     ycoords <- vector()
     for (row in 1:height) {
         for (col in 1:width) {
-            if (da.cube[row,col,gen] == value) {
+            if (da.loaf[row,col,gen] == POP) {
                 xcoords <- c(xcoords,col)
                 ycoords <- c(ycoords,height-row+1)
             }
         }
     }
-    return(list(xcoords,ycoords))
+    plot(xcoords,ycoords,pch=20,col="blue",ylim=c(0,height),xlim=c(0,width))
 }
 
 
-# Plot the points for the generation number passed.
-plot.gen <- function(gen) {
-    Sys.sleep(.4)
-    ppg <- points.for.grid(gen)
-    plot(ppg[[1]],ppg[[2]],xlim=c(1,width),ylim=c(1,height),pch=19,
-        xlab="",ylab="", main="Conway's Game of Life")
-}
 
 
-# Simulate this bad boy.
+# Plot the starting configuration.
+plot.gen(da.loaf,1)
+
+
+# Main loop. For each generation, compute and plot its grid.
 for (gen in 2:num.gen) {
 
-    # Plot the populated points.
-    plot.gen(gen-1)
-
     for (row in 1:height) {
-
+        
         for (col in 1:width) {
 
-            # Get the previous value for this row and column.
-            prevVal <- da.cube[row,col,gen-1]
+            if ((was.just.alive(da.loaf,row,col,gen) && 
+                should.stay.alive(da.loaf,row,col,gen))  ||
+                (!was.just.alive(da.loaf,row,col,gen) && 
+                should.spawn(da.loaf,row,col,gen))) {
 
-            if (prevVal == EMPTY) {
-                if (spawn(row,col,gen-1)) {
-                    da.cube[row,col,gen] <- POPULATED
-                }
-            } else if (stay.alive(row,col,gen-1)) {
-                    da.cube[row,col,gen] <- POPULATED
+                da.loaf[row,col,gen] <- POP                
             }
         }
     }
+    plot.gen(da.loaf,gen)
 }
+
+
+# Plot post-mortem analysis.
+total.pops <- apply(da.loaf,3,sum)
+plot(1:num.gen,total.pops,type="l", main="Total population over time",
+    ylim=c(0,max(total.pops)))
+
