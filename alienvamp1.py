ca63abd965835dcc4496dd8bd9e3695fd24811b1
Stephen <stephen@umw.edu>
2018-02-12 13:16:01 -0500

    Be consistent w/ factor v. rate var names.

--- alienvamp1.py
+++ alienvamp1.py
@@ -9,7 +9,7 @@ delta_t = .1                                    # years
 time_values = np.arange(1940, 2110, delta_t)    # years
 
 abduction_factor = 300   # (abductees/year)/year
-bite_rate = .1           # (vampires/year)/vampire
+bite_factor = .1           # (vampires/year)/vampire
 
 # Create (empty) arrays for the stocks.
 A = np.zeros(len(time_values))
@@ -22,12 +22,9 @@ V[0] = 1
 
 for i in range(1,len(time_values)):
 
-    # Computing single values for A' and V' each time through the loop, since
-    # we don't plan on examining an entire vector of them later.
     A_prime = abduction_factor * i * delta_t
-    V_prime = bite_rate * V[i-1]
+    V_prime = bite_factor * V[i-1]
 
-    # Increase each stock by the appropriate amount for this time step.
     A[i] = A[i-1] + A_prime * delta_t
     V[i] = V[i-1] + V_prime * delta_t
 
